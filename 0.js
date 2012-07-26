6f72522e8e350a814b4c1ab3dce0874a24085feb
Alexander Prinzhorn <alex@nder-prinzhorn.de>
2012-07-26 15:34:12 +0200

    IE event stuff

--- 0.js
+++ 0.js
@@ -1,10 +1,10 @@
 /*! https://github.com/Prinzhorn/0 | free to use under terms of MIT license */
-(function(window, document, documentElement, body) {
+(function(window, document) {
 	var addEvent = function(el, name, fn) {
-		if(el.attachEvent) {
-			el.attachEvent('on' + name, fn);
-		} else {
+		if(el.addEventListener) {
 			el.addEventListener(name, fn, false);
+		} else {
+			el.attachEvent('on' + name, fn);
 		}
 	};
 
@@ -12,11 +12,13 @@
 	box.tabIndex = 100;
 
 	var boxStyle = box.style;
-	boxStyle.cssText = 'position:fixed;left:0;top:0;right:0;bottom:0;z-index:1000;background:rgba(0,0,0,0.7) center no-repeat;background-size:contain;text-align:right;font-size:20px;font-family:sans-serif;font-weight:bold;color:#fff;padding:1em;display:none;cursor:pointer;';
+	boxStyle.cssText = 'position:fixed;left:0;top:0;right:0;bottom:0;z-index:1000;background:#000 center no-repeat;background-color:rgba(0,0,0,0.7);background-size:contain;text-align:right;font-size:20px;font-family:sans-serif;font-weight:bold;color:#fff;padding:1em;display:none;cursor:pointer;';
 	box.innerHTML = 'X';
-	body.appendChild(box);
+	document.body.appendChild(box);
 
-	addEvent(window, 'click', function(e) {
+	addEvent(document, 'click', function(e) {
+		e = e || window.event;
+
 		var el = (e.target || e.srcElement).parentNode;
 
 		if(el.rel === '0') {
@@ -24,7 +26,9 @@
 			boxStyle.backgroundImage = 'url(' + el.href + ')';
 			box.focus();
 
-			(e.preventDefault && e.preventDefault()) || (window.event.preventDefault && window.event.preventDefault()) || (window.event.returnValue = false);
+			e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+			return false;
 		}
 	});
 
@@ -37,4 +41,4 @@
 	addEvent(box, 'click', closeBox);
 	addEvent(box, 'keyup', closeBox);
 
-}(window, document, document.documentElement, document.body));
+}(window, document));
